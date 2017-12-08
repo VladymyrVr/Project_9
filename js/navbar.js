9b7e92b94c63b67e83dfdfaa4932ce1f4825221d
Vova Mahdych <xdrt@ukr.net>
2017-12-08 14:30:53 +0200

    ok google)

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.navbar-toggle, nav').click(function(){
+    $('.navbar-toggle').click(function(){
         $('.navbar-toggle').toggleClass('navbar-on');
         $('.main-nav').fadeToggle();
         $('.main-nav').removeClass('nav-hide');
